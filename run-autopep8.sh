aee38e8191261ed1006599204e3b8eea4d17ba07
Kai Chen <kaichen@mathildetech.com>
2015-05-12 22:29:12 -0700

    remove bogus pep8 exclude

--- run-autopep8.sh
+++ run-autopep8.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/local/bin/autopep8 --verbose --recursive --exclude migrations,settings_docker.py --max-line-length 160 --in-place --aggressive --aggressive .;
+/usr/local/bin/autopep8 --verbose --recursive --max-line-length 160 --in-place --aggressive --aggressive .;
 
 
