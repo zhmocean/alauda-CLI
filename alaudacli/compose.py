dca05783a532f1a520ccf2f94d6333fd7597be73
Kai Chen <kaichen@mathildetech.com>
2015-05-13 17:54:05 -0700

    compose - load yaml file

--- alaudacli/compose.py
+++ alaudacli/compose.py
@@ -1,5 +1,25 @@
+import sys
+import yaml
+
 from project import Project
 
 
 def load_project(filepath):
-    return Project()
+    compose_data = _load_yaml(filepath)
+    services = load_services(compose_data)
+    project = Project(services)
+    return project
+
+
+def load_services(compose_data):
+    services = []
+    return services
+
+
+def _load_yaml(filepath):
+    try:
+        with open(filepath, 'r') as f:
+            return yaml.safe_load(f)
+    except:
+        print 'Missing or invalid compose yaml file. (Do you have docker-compose.yml in the current directory?)'
+        sys.exit(1)
--- alaudacli/project.py
+++ alaudacli/project.py
@@ -1,6 +1,6 @@
 class Project(object):
-    def __init__(self):
-        pass
+    def __init__(self, services):
+        self.services = services
 
     def up(self):
         pass
