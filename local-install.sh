ddebc3565fbdd4118edd53f60c2738ec114f3374
XiaodongZhang <xdzhang@mathildetech.com>
2015-05-19 21:49:53 +0800

    support completion

--- local-install.sh
+++ local-install.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo rm -r -f /usr/local/bin/alaudacli
 sudo rm /usr/local/bin/alauda
 sudo cp -r alaudacli /usr/local/bin
 sudo cp bin/alauda /usr/local/bin
 
+sudo rm /etc/bash_completion.d/alauda
+sudo cp ./alauda /etc/bash_completion.d
+
+. /etc/bash_completion.d/alauda
+
+echo "Please re-login to finish the install!"
